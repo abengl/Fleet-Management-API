4261b76f84765498fbb31a80032bed7ba48fdd06
Aben <126703861+abengl@users.noreply.github.com>
2024-08-31 18:11:55 -0500

    update: deleted javadoc and update annotations #5

--- src/main/java/com/fleetmanagement/api_rest/model/Taxi.java
+++ src/main/java/com/fleetmanagement/api_rest/model/Taxi.java
@@ -1,35 +1,33 @@
 package com.fleetmanagement.api_rest.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
-/**
- * Represents a taxi entity that is mapped to the "taxis" table in the database.
- * This entity has two attributes: `id` (the primary key) and `plate` (the taxi's license plate).<br>
- * <p>
- * This class is managed by Hibernate through the Java Persistence API (JPA) and
- * can be used for CRUD operations within the Spring Boot framework.<br>
- */
 @Entity
 @Table(name = "taxis")
 @Getter @Setter
 public class Taxi {
 
 	@Id
-	private int id;
+	private Integer id;
 
 	@Column(name = "plate")
 	private String plate;
 
-	@OneToMany(mappedBy = "taxi_id", cascade = CascadeType.ALL, orphanRemoval = true)
-	@JsonBackReference
+	@OneToMany(mappedBy = "taxiId", cascade = CascadeType.ALL, orphanRemoval = true)
+	@JsonManagedReference
 	private List<Trajectory> trajectories;
 
 	public Taxi() {
 	}
 
+	public Taxi(Integer id, String plate, List<Trajectory> trajectories) {
+		this.id = id;
+		this.plate = plate;
+		this.trajectories = trajectories;
+	}
 }
