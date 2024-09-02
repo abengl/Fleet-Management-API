e0e2bec51dc41dff190e8491133ceadca7ade5d0
Aben <126703861+abengl@users.noreply.github.com>
2024-09-02 13:06:13 -0500

    feature: latest trajectory endpoint controller #3

--- src/main/java/com/fleetmanagement/api_rest/controller/TrajectoryController.java
+++ src/main/java/com/fleetmanagement/api_rest/controller/TrajectoryController.java
@@ -1,7 +1,7 @@
 package com.fleetmanagement.api_rest.controller;
 
+import com.fleetmanagement.api_rest.dto.LatestTrajectoryDTO;
 import com.fleetmanagement.api_rest.dto.TrajectoryDTO;
-import com.fleetmanagement.api_rest.exception.InvalidFormatException;
 import com.fleetmanagement.api_rest.service.TrajectoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -26,14 +25,20 @@ public class TrajectoryController {
 
 	@GetMapping
 	public ResponseEntity<List<TrajectoryDTO>> getAllTrajectoriesByIdAndPlate(
-			@RequestParam(name = "taxiId") Integer taxiId, @RequestParam(name = "date") String date,
+			@RequestParam(name = "taxiId", required = false) Integer taxiId,
+			@RequestParam(name = "date", required = false) String date,
 			@RequestParam(name = "page", defaultValue = "0") int page,
 			@RequestParam(name = "limit", defaultValue = "20") int limit) {
-		try {
+
 			List<TrajectoryDTO> trajectories = trajectoryService.getTrajectories(taxiId, date, page, limit);
 			return ResponseEntity.ok(trajectories);
-		} catch (ParseException e) {
-			throw new InvalidFormatException("The date format should be dd-MM-yyyy");
-		}
+	}
+
+	@GetMapping("/latest")
+	public ResponseEntity<List<LatestTrajectoryDTO>> getLatestTrajectories(
+			@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "limit", defaultValue = "20") int limit) {
+		List<LatestTrajectoryDTO> latestTrajectories = trajectoryService.getLatestTrajectories(page, limit);
+		return ResponseEntity.ok(latestTrajectories);
 	}
 }
