d947ef67344efcb0751982199701cb7307c7f7c0
Aben <126703861+abengl@users.noreply.github.com>
2024-09-02 16:20:24 -0500

    fix: dependency error
    dependency missing and context loading for unit test #5

--- src/test/java/com/fleetmanagement/api_rest/ApiRestApplicationTest.java
+++ src/test/java/com/fleetmanagement/api_rest/ApiRestApplicationTest.java
@@ -1,18 +1,18 @@
 package com.fleetmanagement.api_rest;
 
-import com.fleetmanagement.api_rest.controller.TaxiController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class ApiRestApplicationTest {
-	@Autowired
-	TaxiController taxiController;
+@ActiveProfiles("default")
+class ApiRestApplicationTests {
 
 	@Test
-	public void contextLoads() {
-		assertThat(taxiController).isNot(null);
+	void contextLoads() {
+		System.out.println("DB_URL: " + System.getenv("DB_URL"));
+		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
 	}
+
 }
