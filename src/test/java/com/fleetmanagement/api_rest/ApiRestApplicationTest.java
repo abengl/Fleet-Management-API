045aa6a95e6f7446d892c976fe57d15539934bac
Aben <126703861+abengl@users.noreply.github.com>
2024-09-02 20:08:51 -0500

    test(taxi): app unit test with junit #5

--- src/test/java/com/fleetmanagement/api_rest/ApiRestApplicationTest.java
+++ src/test/java/com/fleetmanagement/api_rest/ApiRestApplicationTest.java
@@ -2,17 +2,12 @@ package com.fleetmanagement.api_rest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("default")
 class ApiRestApplicationTests {
 
 	@Test
-	void contextLoads() {
-		System.out.println("DB_URL: " + System.getenv("DB_URL"));
-		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
+	public void contextLoads() {
 	}
 
 }
