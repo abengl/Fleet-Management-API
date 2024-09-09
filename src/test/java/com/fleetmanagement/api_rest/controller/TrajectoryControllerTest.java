08f1272f6822be0412fcc923dfda1b46371bded7
Aben <126703861+abengl@users.noreply.github.com>
2024-09-09 17:26:12 -0500

    enhance(trajectory): imports deleted #4

--- src/test/java/com/fleetmanagement/api_rest/controller/TrajectoryControllerTest.java
+++ src/test/java/com/fleetmanagement/api_rest/controller/TrajectoryControllerTest.java
@@ -2,7 +2,6 @@ package com.fleetmanagement.api_rest.controller;
 
 import com.fleetmanagement.api_rest.dto.LatestTrajectoryDTO;
 import com.fleetmanagement.api_rest.dto.TrajectoryDTO;
-import com.fleetmanagement.api_rest.model.Trajectory;
 import com.fleetmanagement.api_rest.service.TrajectoryService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,16 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.*;
+import java.util.List;
 
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
