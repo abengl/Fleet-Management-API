65be8b3c042bf2dafcc89cd5b986b72ac73c0c16
Aben <126703861+abengl@users.noreply.github.com>
2024-09-15 21:18:00 -0500

    test: service unit test
    - Enhance taxi service test #5
    - Enhance trajectory unit test #4
    - Service unit tests for users completed #35

--- src/test/java/com/fleetmanagement/api_rest/business/service/TrajectoryServiceTest.java
+++ src/test/java/com/fleetmanagement/api_rest/business/service/TrajectoryServiceTest.java
@@ -1,32 +1,31 @@
 package com.fleetmanagement.api_rest.business.service;
 
-import com.fleetmanagement.api_rest.presentation.dto.LatestTrajectoryDTO;
-import com.fleetmanagement.api_rest.presentation.dto.TrajectoryDTO;
+
 import com.fleetmanagement.api_rest.business.exception.InvalidFormatException;
 import com.fleetmanagement.api_rest.business.exception.ValueNotFoundException;
-import com.fleetmanagement.api_rest.presentation.mapper.LatestTrajectoryMapper;
-import com.fleetmanagement.api_rest.presentation.mapper.TrajectoryMapper;
 import com.fleetmanagement.api_rest.persistence.entity.Taxi;
 import com.fleetmanagement.api_rest.persistence.entity.Trajectory;
 import com.fleetmanagement.api_rest.persistence.repository.TaxiRepository;
 import com.fleetmanagement.api_rest.persistence.repository.TrajectoryRepository;
+import com.fleetmanagement.api_rest.presentation.dto.LatestTrajectoryDTO;
+import com.fleetmanagement.api_rest.presentation.dto.TrajectoryDTO;
+import com.fleetmanagement.api_rest.presentation.mapper.LatestTrajectoryMapper;
+import com.fleetmanagement.api_rest.presentation.mapper.TrajectoryMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.security.InvalidParameterException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -37,8 +36,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TrajectoryServiceTest {
 
 	@Mock
@@ -52,19 +49,21 @@ class TrajectoryServiceTest {
 	@InjectMocks
 	TrajectoryService trajectoryService;
 
+	String dateString = "2024-01-01";
+	double latitude = 100.00;
+	double longitude = -100.00;
+	private int page = 0;
+	private int limit = 10;
+
 	@Test
 	@DisplayName("Testing getTrajectories() without taxiId - It throws a RequiredParameterException")
 	public void getTrajectoriesWithoutTaxiIdTest() {
-
 		// Arrange
-		Integer taxiId = null;
-		String stringDate = "2024-01-01";
-		int page = 0;
-		int limit = 10;
+		Integer undefinedId = null;
 
 		// Act & Assert
 		InvalidParameterException exception = assertThrows(InvalidParameterException.class,
-				() -> trajectoryService.getTrajectories(taxiId, stringDate, page, limit));
+				() -> trajectoryService.getTrajectories(undefinedId, dateString, page, limit));
 		assertEquals("Missing taxiId value.", exception.getMessage());
 	}
 
@@ -72,17 +71,14 @@ class TrajectoryServiceTest {
 	@DisplayName("Testing getTrajectories() with non-existent taxiId - It throws a ValueNotFoundException")
 	public void getTrajectoriesNonExistentTaxiIdTest() {
 		// Arrange
-		Integer taxiId = 1111111;
-		String stringDate = "2024-01-01";
-		int page = 0;
-		int limit = 10;
+		Integer invalidId = 1111111;
 
-		when(taxiRepository.existsById(taxiId)).thenReturn(false);
+		when(taxiRepository.existsById(invalidId)).thenReturn(false);
 
 		// Act & Assert
 		ValueNotFoundException exception = assertThrows(ValueNotFoundException.class,
-				() -> trajectoryService.getTrajectories(taxiId, stringDate, page, limit));
-		assertEquals("Taxi ID " + taxiId + " not found.", exception.getMessage());
+				() -> trajectoryService.getTrajectories(invalidId, dateString, page, limit));
+		assertEquals("Taxi ID " + invalidId + " not found.", exception.getMessage());
 	}
 
 	@Test
@@ -90,15 +86,13 @@ class TrajectoryServiceTest {
 	public void getTrajectoriesWithoutDateTest() {
 		// Arrange
 		Integer taxiId = 1;
-		String stringDate = "";
-		int page = 0;
-		int limit = 10;
+		String emptyDate = "";
 
 		when(taxiRepository.existsById(taxiId)).thenReturn(true);
 
 		// Act & Assert
 		InvalidParameterException exception = assertThrows(InvalidParameterException.class,
-				() -> trajectoryService.getTrajectories(taxiId, stringDate, page, limit));
+				() -> trajectoryService.getTrajectories(taxiId, emptyDate, page, limit));
 		assertEquals("Missing date value.", exception.getMessage());
 	}
 
@@ -107,44 +101,44 @@ class TrajectoryServiceTest {
 	public void getTrajectoriesInvalidDateTest() {
 		// Arrange
 		Integer taxiId = 1;
-		String stringDate = "123xyz";
-		int page = 0;
-		int limit = 10;
+		String invalidDate = "123xyz";
 
 		when(taxiRepository.existsById(taxiId)).thenReturn(true);
 
 		// Act & Assert
 		InvalidFormatException exception = assertThrows(InvalidFormatException.class,
-				() -> trajectoryService.getTrajectories(taxiId, stringDate, page, limit));
-		assertEquals("Incorrect date value: " + stringDate, exception.getMessage());
+				() -> trajectoryService.getTrajectories(taxiId, invalidDate, page, limit));
+		assertEquals("Incorrect date value: " + invalidDate, exception.getMessage());
 	}
 
 	@Test
 	@DisplayName("Testing getTrajectories() with valid parameters - It returns a Trajectory page")
 	public void getTrajectoriesTest() throws ParseException {
-
 		// Arrange
 		Integer taxiId = 1;
-		String dateString = "2024-01-01";
+
 		SimpleDateFormat formatStringToDate = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
 		Date date = formatStringToDate.parse(dateString);
 
-		int page = 0;
-		int limit = 10;
-		double latitude = 100.00;
-		double longitude = -100.00;
+		Taxi taxi1 = Taxi.builder().plate("ABC-123").build();
 
-		Taxi taxi1 = new Taxi(taxiId, "ABC-123", new ArrayList<>());
+		List<Trajectory> trajectories =
+				Arrays.asList(
+						Trajectory.builder().taxiId(taxi1).date(date).latitude(latitude).longitude(longitude).build());
 
-		List<Trajectory> trajectories = List.of(new Trajectory(10, taxi1, date, latitude, longitude));
 		Pageable pageable = PageRequest.of(page, limit);
 		Page<Trajectory> trajectoriesPage = new PageImpl<>(trajectories, pageable, trajectories.size());
 
-		// Mock the repository and mapper behaviors
+		TrajectoryDTO trajectory = TrajectoryDTO.builder()
+				.plate("ABC-123")
+				.taxiId(taxiId)
+				.date(dateString)
+				.latitude(latitude)
+				.longitude(longitude).build();
+
 		when(taxiRepository.existsById(taxiId)).thenReturn(true);
 		when(trajectoryRepository.findByTaxiId_IdAndDate(taxiId, date, pageable)).thenReturn(trajectoriesPage);
-		when(trajectoryMapper.toTrajectoryDTO(any(Trajectory.class))).thenReturn(
-				new TrajectoryDTO(taxiId, "ABC-123", taxiId, dateString, latitude, longitude));
+		when(trajectoryMapper.toTrajectoryDTO(any(Trajectory.class))).thenReturn(trajectory);
 
 		// Act
 		List<TrajectoryDTO> trajectoryDTOList = trajectoryService.getTrajectories(taxiId, dateString, page, limit);
@@ -162,25 +156,31 @@ class TrajectoryServiceTest {
 	@DisplayName("Testing getLatestTrajectories() with valid parameter - It returns a Trajectory page")
 	public void getLatestTrajectoriesTest() throws ParseException {
 		// Arrange
-		String dateString = "2024-01-01";
+		Integer taxiId = 1;
+
 		SimpleDateFormat formatStringToDate = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
 		Date date = formatStringToDate.parse(dateString);
 
-		int page = 0;
-		int limit = 10;
-		double latitude = 100.00;
-		double longitude = -100.00;
+		Taxi taxi1 = Taxi.builder().plate("ABC-123").build();
 
-		Taxi taxi1 = new Taxi(1, "ABC-123", new ArrayList<>());
+		List<Trajectory> trajectories =
+				Arrays.asList(
+						Trajectory.builder().taxiId(taxi1).date(date).latitude(latitude).longitude(longitude).build());
 
-		List<Trajectory> trajectories = List.of(new Trajectory(10, taxi1, date, latitude, longitude));
 		Pageable pageable = PageRequest.of(page, limit);
 		Page<Trajectory> trajectoriesPage = new PageImpl<>(trajectories, pageable, trajectories.size());
+
+		LatestTrajectoryDTO latestTrajectory = LatestTrajectoryDTO.builder()
+				.taxiId(taxiId)
+				.plate("ABC-123")
+				.date(dateString)
+				.latitude(latitude)
+				.longitude(longitude).build();
 
 		// Mock the repository and mapper
 		when(trajectoryRepository.findLatestLocations(pageable)).thenReturn(trajectoriesPage);
 		when(latestTrajectoryMapper.toLatestTrajectoryDTO(any(Trajectory.class))).thenReturn(
-				new LatestTrajectoryDTO(10, "ABC-123", dateString, latitude, longitude));
+				latestTrajectory);
 
 		// Act
 		List<LatestTrajectoryDTO> latestTrajectoriesDTOList = trajectoryService.getLatestTrajectories(page, limit);
@@ -192,3 +192,4 @@ class TrajectoryServiceTest {
 		verify(latestTrajectoryMapper).toLatestTrajectoryDTO(any(Trajectory.class)); // Called once
 	}
 }
+
