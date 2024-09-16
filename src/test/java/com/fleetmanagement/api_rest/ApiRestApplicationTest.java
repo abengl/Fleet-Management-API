01116b925e075d8e95b07f4de80c90f5360a9e62
Aben <126703861+abengl@users.noreply.github.com>
2024-09-16 18:02:33 -0500

    enhance: database credentials

--- src/test/java/com/fleetmanagement/api_rest/ApiRestApplicationTest.java
+++ src/test/java/com/fleetmanagement/api_rest/ApiRestApplicationTest.java
@@ -2,8 +2,10 @@ package com.fleetmanagement.api_rest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiRestApplicationTest {
 
 	@Test
