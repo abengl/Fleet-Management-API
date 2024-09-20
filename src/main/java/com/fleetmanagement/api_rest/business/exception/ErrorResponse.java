dbe0c734c45464380c32ff06e76781ffa45e89d4
Aben <126703861+abengl@users.noreply.github.com>
2024-09-19 20:14:12 -0500

    feature: login endpoint implementation #47
    - error handling for missing parameters or invalid credentials

--- src/main/java/com/fleetmanagement/api_rest/business/exception/ErrorResponse.java
+++ src/main/java/com/fleetmanagement/api_rest/business/exception/ErrorResponse.java
@@ -6,9 +6,15 @@ import lombok.Setter;
 @Getter @Setter
 public class ErrorResponse {
 	private String error;
+	//private Map<String, String> details;
 
 	public ErrorResponse(String error) {
 		this.error = error;
 	}
+
+	/*public ErrorResponse(String error, Map<String, String> details) {
+		this.error = error;
+		this.details = details;
+	}*/
 
 }
